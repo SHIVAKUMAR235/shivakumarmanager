c484483845d659a663ac68a9609afa8f3540d2a2
SHIVAKUMAR235 <101509161+SHIVAKUMAR235@users.noreply.github.com>
2022-05-04 19:31:11 +0530

    Update Logout.java
    commited by shivakumar

--- src/test/java/pageclasses/Logout.java
+++ src/test/java/pageclasses/Logout.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Logout 
 {
-	
+	//locating elements
 	@FindBy(xpath="//div[text()='Logout']") public WebElement logout;
 	
 	public Logout(RemoteWebDriver driver)
